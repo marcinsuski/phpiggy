5b5f6a08b4a9bf11302ceb741e80b7f1202a43fe
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2024-02-20 18:47:08 +0100

    Update

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,7 +5,16 @@ declare(strict_types=1);
 namespace Framework;
 
 class App {
+    private Router $router;
+
+    public function __construct() {
+        $this->router = new Router();
+    }
     public function run() {
         echo "Application is running";
+    }
+
+    public function get(string $path, array $controller) {
+        $this->router->add('GET', $path, $controller);
     }
 }
