b974e0f59d005d6d6e22910e0add47ed13e030d4
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2024-02-28 18:49:08 +0100

    Add form validation and error handling

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\TemplateDataMiddleware;
+use App\Middleware\{SessionMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware};
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
+    $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(SessionMiddleware::class);
 }
